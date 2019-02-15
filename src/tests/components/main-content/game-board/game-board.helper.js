e07ebb32ef561ed0a51b7496e24e68eb9cc689fe
Stefano <stefan.emailacct@gmail.com>
2019-02-15 12:25:15 +0000

    [test]currentGame component test

---
+++ src/tests/components/main-content/game-board/game-board.helper.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const currentGame = ({currentGameResult}) => {
+  if(currentGameResult !== null) {
+    if(currentGameResult === 'wins') {
+      return (
+        <div className='current-game'>
+          <h3 className='text-center'>You won!</h3>
+        </div>
+      );
+    } else
+    if(currentGameResult === 'losses') {
+      return (
+        <div className='current-game'>
+          <h3 className='text-center'>You lost!</h3>
+        </div>
+      );
+    } else
+    if(currentGameResult === 'draws') {
+      return (
+        <div className='current-game'>
+          <h3 className='text-center'>You draw!</h3>
+        </div>
+      );
+    }
+  }
+  else if(currentGameResult === null) {
+    return (
+      <div className='current-game'>
+        <h3 className='text-center'>Click on an opponent, and pick your hand to begin.</h3>
+      </div>
+    );
+  }
+};
