75085fa756f7ee14e74fa4283c02b3fb112711e0
Stefano <stefan.emailacct@gmail.com>
2019-02-18 23:43:45 +0000

    [test]dashboard isLoading

--- src/tests/components/main-content/Dashboard.test.js
+++ src/tests/components/main-content/Dashboard.test.js
@@ -4,13 +4,44 @@ import Dashboard from  '../../../components/main-content/Dashboard';
 import toJSON from 'enzyme-to-json';
 
 describe('Dashboard component', () => {
-  it('should render dashboard page correctly', () => {
-    const wrapper = shallow(<Dashboard />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
+  describe('isLoading set to true', () => {
+    it('should render dashboard page correctly', () => {
+      const wrapper = shallow(<Dashboard />);
+      expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+    it('should have class of "mid-section-wrapper"', () => {
+      const wrapper = shallow(<Dashboard />);
+      expect(wrapper.find('.mid-section-wrapper').length).toBe(1);
+    });
+    it('should have state "isLoading" set to true', () => {
+      const wrapper = shallow(<Dashboard />);
+      expect(wrapper.state('isLoading')).toBe(true);
+    });
+
   });
-  it('should have class of "mid-section-wrapper"', () => {
-    const wrapper = shallow(<Dashboard />);
-    expect(wrapper.find('.mid-section-wrapper').length).toBe(1);
+  describe('isLoading set to false', () => {
+    it('should render dashboard page correctly', () => {
+      const wrapper = shallow(<Dashboard />);
+      wrapper.setState({
+        isLoading: false,
+      });
+      expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+    it('should have class of "mid-section-wrapper"', () => {
+      const wrapper = shallow(<Dashboard />);
+      wrapper.setState({
+        isLoading: false,
+      });
+      expect(wrapper.find('.mid-section-wrapper').length).toBe(1);
+    });
+    it('should have state "isLoading" set to false', () => {
+      const wrapper = shallow(<Dashboard />);
+      wrapper.setState({
+        isLoading: false,
+      });
+      expect(wrapper.state('isLoading')).toBe(false);
+    });
+
   });
 
 });
